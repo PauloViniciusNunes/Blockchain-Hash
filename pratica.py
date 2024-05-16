0bbb0e247b3c43a915b78c4f5bb218552857c38e
paulo_vinus <152558820+PauloViniciusNunes@users.noreply.github.com>
2024-05-16 14:28:02 -0300

    Add código .py e executável

---
+++ pratica.py
@@ -0,0 +1,31 @@
+import time
+from random import randint as rant
+from os import system as sys
+
+alfa = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+hash = ''
+sys('color a')
+dificuldade = int(input("Defina a dificuldade: "))
+for i in range(0, dificuldade):
+
+    hash += alfa[rant(0, len(alfa) - 1)]
+
+inicio = time.time()
+
+while True:
+    hash_key = ''
+    for j in range(0, dificuldade):
+        hash_key += alfa[rant(0, len(alfa) - 1)]
+    print(hash_key)
+    if hash_key == hash:
+        sys('cls')
+        break
+
+
+fim = time.time()
+
+minutos = int((fim-inicio)/60)
+segundos = int((fim-inicio)%60)
+print(f"HASH DESCOBERTO: {hash_key}")
+print(f"Tempo para quebrar o hash: {minutos} minutos e {segundos} segundos")
+input()
